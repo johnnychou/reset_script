64d2dbd2699ef4070e5d7b85d308cc961ea2fe59
JohnnyChou <johnny800503.work@gmail.com>
2017-03-23 11:41:34 +0800

    re-write reset script

--- reset.py
+++ reset.py
@@ -9,45 +9,33 @@ cli_init = {
 }
 
 # show_map = cli.ShowMap(cli_init)
-delete_map = cli.DeleteMap(cli_init)
-delete_part = cli.DeletePartition(cli_init)
 show_si = cli.ShowSnapshot(cli_init)
 show_part = cli.ShowPartition(cli_init)
+show_replica = cli.ShowReplica(cli_init)
+
 delete_si = cli.DeleteSnapshot(cli_init)
+delete_part = cli.DeletePartition(cli_init)
+delete_replica = cli.DeleteReplica(cli_init)
 delete_map = cli.DeleteMap(cli_init)
 
-# print(show_map.execute())
-
-# print(len(list(range(0))))
-
-rc, part_list = show_part.execute()
+rc, replica_list = show_replica.execute()
+rc, part_list = show_part.execute('-l')
 rc, si_list = show_si.execute()
 
-print(part_list)
+print('Parts: %s' % part_list)
 
-for entry in part_list:
-    part_id = entry['ID']
+for replica in replica_list:
+    print('Delete Replica ID: %s' % replica['Pair-ID'])
+    rc, out = delete_replica.execute(replica['Pair-ID'], '-y')
 
-    if len(entry['Name']) == 32:
-        print('Start Delete Part: %s' % part_id)
+for si in si_list:
+    print('Delete SI ID: %s' % si['SI-ID'])
+    rc, out = delete_si.execute(si['SI-ID'], '-y')
 
-        for si_entry in si_list:
-            if si_entry['Partition-ID'] == entry['ID']:
-                print('Delete SI ID: %s' % si_entry['SI-ID'])
-                delete_si.execute(si_entry['SI-ID'], '-y')
+for part in part_list:
+    if entry['Mapped'].lower() == 'true':
+        print('Delete Map for Part ID: %s' % part['ID'])
+        rc, out = delete_map.execute('part',  part['ID'], '-y')
+    print('Delete Part ID: %s' % part['ID'])
+    rc, out = delete_part.execute(part['ID'], '-y')
 
-        delete_map.execute('part', part_id, '-y')
-
-        print('End Delete Part: %s' % part_id)
-        rc, out = delete_part.execute(part_id, '-y')
-
-# Test = [1, 2, 3, 4]
-
-# # Test.remove(1)
-# if 1 in Test:
-#     Test.remove(1)
-# print(Test)
-
-# if 1 in Test:
-#     Test.remove(1)
-# print(Test)
